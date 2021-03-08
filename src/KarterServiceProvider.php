3060740a39f5d0d8430a15aaed7f568a93dbf43e
Félix Dorn <github@felixdorn.fr>
2021-03-08 07:21:25 +0100

    :sparkles: add title component

--- src/KarterServiceProvider.php
+++ src/KarterServiceProvider.php
@@ -2,17 +2,19 @@
 
 namespace Starts\Karter;
 
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\ServiceProvider;
-use Starts\Karter\Components\Form;
+use Symfony\Component\Finder\SplFileInfo;
 
 class KarterServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        $this->loadViewsFrom(__DIR__.'/../resources/views', 'karter');
-        $this->loadViewComponentsAs('karter', [
-            Form::class,
-        ]);
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'karter');
+        $this->loadViewComponentsAs('karter',
+            collect(File::allFiles(__DIR__ . '/Components/')
+            )->map(fn (SplFileInfo $file) => "Starts\\Karter\\Components\\{$file->getFilenameWithoutExtension()}")
+                ->toArray());
 
         if (!$this->app->runningInConsole()) {
             return;
