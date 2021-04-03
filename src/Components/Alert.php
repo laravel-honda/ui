c47d5206f1ec2c593f295a7922af0eb4a1eb9fed
Félix Dorn <github@felixdorn.fr>
2021-04-03 11:31:43 +0000

    :sparkles: update alert component

--- src/Components/Alert.php
+++ src/Components/Alert.php
@@ -7,24 +7,29 @@ use Illuminate\View\Component;
 
 class Alert extends Component
 {
-    public string $content;
+    public ?string $content;
     public string $icon;
     public bool $closeable;
     public string $type;
     public string $description;
 
-    public function __construct(string $type, string $content = null, string $description = '', string $icon = 'information-circle', bool $closeable = false, bool $inSession = false)
+    public function __construct(string $type, string $content = null, string $description = '', string $icon = 'info-circle', bool $closeable = false, bool $inSession = false)
     {
         $this->content = $inSession ? session($type) : $content;
-        $this->type    = [
-                'error'   => 'red',
+        $this->type = [
+                'error' => 'red',
                 'success' => 'green',
                 'warning' => 'yellow',
-                'info'    => 'blue',
+                'info' => 'blue',
             ][$type] ?? 'gray';
         $this->description = $description;
-        $this->icon        = $icon;
-        $this->closeable   = $closeable;
+        $this->icon = $icon;
+        $this->closeable = $closeable;
+    }
+
+    public function shouldRender(): bool
+    {
+        return !empty($this->content);
     }
 
     public function render(): View
