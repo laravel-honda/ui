e01687caa62e4ed41e73baf2dda461cdcc08e7b8
Félix Dorn <github@felixdorn.fr>
2021-06-30 09:35:28 +0000

    Update input.blade.php

--- resources/views/input.blade.php
+++ resources/views/input.blade.php
@@ -6,7 +6,7 @@
     <div class="flex items-stretch @if (!$hideLabel) mt-1 @endif @if($inline) ml-4 @endif">
         @if(isset($outsideLeft) || ($icon->isNotNull() && $iconSide->is('left')))
             <div
-                class="border border-gray-300 bg-gray-100  rounded-lg rounded-r-none whitespace-nowrap py-2 flex items-center">
+                class="border border-gray-300 bg-gray-100  rounded-lg rounded-r-none whitespace-nowrap flex items-center">
                 @if($icon->isNotNull())
                     <div class="px-3 mt-0.5">
                         {{ $icon->with(['class' => 'w-5 h-5 text-gray-700']) }}
@@ -18,7 +18,7 @@
             </div>
         @endisset
         @isset($insideLeft)
-            <div class="border border-gray-300 rounded-lg border-r-0 rounded-r-none whitespace-nowrap py-2">
+            <div class="border border-gray-300 rounded-lg border-r-0 rounded-r-none whitespace-nowrap">
                 {{ $insideLeft }}
             </div>
         @endisset
@@ -30,13 +30,13 @@
     "rounded-r-none" => isset($outsideRight) || ($icon->isNotNull() && $iconSide->is('right')),
 ]) }} @if ($disabled) disabled aria-disabled="true" @endif @if ($readonly) readonly aria-readonly="true" @endif>
         @isset($insideRight)
-            <div class="border border-gray-300 rounded-lg border-l-0 rounded-l-none whitespace-nowrap py-2">
+            <div class="border border-gray-300 rounded-lg border-l-0 rounded-l-none whitespace-nowrap">
                 {{ $insideRight }}
             </div>
         @endisset
         @if(isset($outsideRight) || ($icon->isNotNull() && $iconSide->is('right')))
             <div
-                class="border border-gray-300 bg-gray-100  rounded-lg border-l-0 rounded-l-none whitespace-nowrap py-2 flex items-center">
+                class="border border-gray-300 bg-gray-100  rounded-lg border-l-0 rounded-l-none whitespace-nowrapflex items-center">
                 @if($icon->isNotNull())
                     <div class="px-3 mt-0.5">
                         {{ $icon->with(['class' => 'w-5 h-5 text-gray-700']) }}
