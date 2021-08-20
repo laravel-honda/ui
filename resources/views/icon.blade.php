899155220f56bf65b734f9f34d181c3babc21e6e
Félix Dorn <github@felixdorn.fr>
2021-08-20 20:52:05 +0200

    fix icon component

--- resources/views/icon.blade.php
+++ resources/views/icon.blade.php
@@ -1,3 +1,3 @@
 @php($classes = $size !== null ? sprintf('w-%s h-%s ', $size, $size) : '')
 
-{{ svg($name, $classes . $attributes->get('class'), $attributes->except('class')->toArray()) }}
+{{ svg($name, $classes . $attributes->get('class'), $attributes->except('class')->getAttributes()) }}
