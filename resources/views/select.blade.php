5c0fb8e30e5e5399d464d4c8dae1a1ad671315d8
Félix Dorn <github@felixdorn.fr>
2021-08-21 15:22:04 +0000

    Update select.blade.php

--- resources/views/select.blade.php
+++ resources/views/select.blade.php
@@ -1,6 +1,6 @@
 <div class="@if ($inline) flex @endif @if (!$first) mt-4 @endif">
     @if (!$hideLabel || !$inline)
-        <label for="{{ $name }}">{{ $label }}</label>
+        <label class="text-gray-700" for="{{ $name }}">{{ $label }}</label>
     @endif
     <select name="{{ $name }}"
             id="{{ $name }}" {{ $attributes->class("block w-full mt-1 rounded-md border-gray-300 shadow-sm focus:outline-none focus:ring-$color-500 focus:border-$color-500") }}>
